07f5e8f94a6bed8328475d485452fe12271e5756
2DC <kkts9308@gmail.com>
2024-02-09 20:57:04 +0900

    🎨 Auth: githubUserInfo에 타입 설정 및 수도코드 주석 작성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import {
   GITHUB_GET_TOKEN_URL,
   GITHUB_GET_USER_INFO_URL,
 } from 'src/common/constants/env-keys.const';
+import { UsersModel } from 'src/users/entities/users.entity';
 
 @Injectable()
 export class AuthService {
@@ -15,10 +16,21 @@ export class AuthService {
   async loginGithubOauth(authCode: string) {
     const githubAccessToken = await this.getGitHubAccessToken(authCode);
     const githubUserInfo = await this.getGitHubUserInfo(githubAccessToken);
+    // DB에 githubId가 등록되었는지 안되었는지 확인
+    // 안되었다면 DB에 유저 정보 등록
+    // 이후 토큰 발급 (access, refresh)
+
     return githubUserInfo;
   }
 
-  async getGitHubUserInfo(accessToken: string) {
+  async getGitHubUserInfo(
+    accessToken: string,
+  ): Promise<
+    Pick<
+      UsersModel,
+      'githubId' | 'loginId' | 'avatar' | 'nickname' | 'githubUrl'
+    >
+  > {
     const getUserInfoUrl = this.configService.get(GITHUB_GET_USER_INFO_URL);
     const response: AxiosResponse = await axios.get(getUserInfoUrl, {
       headers: {
@@ -37,7 +49,7 @@ export class AuthService {
       loginId: login,
       avatar: avatar_url,
       nickname: name,
-      url: html_url,
+      githubUrl: html_url,
     };
   }
 
